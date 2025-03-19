72291772b9141eb3b8dabf34c2c5fadf1c8bf1a1
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-03-19 18:31:19 +0100

    feat: change layout to show error dialog if logout or login failed

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -2,11 +2,13 @@ import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {User} from '../../models/user.model';
 import {AuthService} from '../../services/authentication/auth.service';
+import {ErrorDialogComponent} from '../../components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-main-layout',
     imports: [
-        RouterOutlet
+        RouterOutlet,
+        ErrorDialogComponent
     ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
@@ -16,6 +18,7 @@ export class MainLayoutComponent implements OnInit {
   isLoggedIn = false;
   user: User | null = null;
   errorMessage: string | null = null;
+  showErrorDialog = false;
 
   private readonly authService = inject(AuthService);
 
@@ -35,6 +38,7 @@ export class MainLayoutComponent implements OnInit {
       this.isLoggedIn = await this.authService.isAuthenticated();
       if (this.isLoggedIn) this.user = await this.authService.getUser();
     } catch (error) {
+      console.error("That shouldn't have happened.");
       this.handleError(error);
     }
   }
@@ -56,6 +60,11 @@ export class MainLayoutComponent implements OnInit {
     } else {
       this.errorMessage = 'An unknown error occurred';
     }
+    this.showErrorDialog = true;
     console.error('Authentication error:', error);
+  }
+
+  closeErrorDialog() {
+    this.showErrorDialog = false;
   }
 }
