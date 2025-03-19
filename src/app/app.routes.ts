64547beba978140c51af6e7ab5734241763be53a
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-03-19 17:11:39 +0100

    feat: adding layout and default page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+    ]
+  }
+];
+
