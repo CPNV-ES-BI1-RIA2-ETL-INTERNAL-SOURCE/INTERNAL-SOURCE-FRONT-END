f6d489930e6999e88bd8210328cae3a3fd0a6392
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-03-19 17:12:12 +0100

    feat: adding layout component

---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,61 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {User} from '../../models/user.model';
+import {AuthService} from '../../services/authentication/auth.service';
+
+@Component({
+  selector: 'app-main-layout',
+    imports: [
+        RouterOutlet
+    ],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent implements OnInit {
+  title = 'ria-frontend';
+  isLoggedIn = false;
+  user: User | null = null;
+  errorMessage: string | null = null;
+
+  private readonly authService = inject(AuthService);
+
+  async ngOnInit() {
+    try {
+      this.isLoggedIn = await this.authService.isAuthenticated();
+      if (this.isLoggedIn) this.user = await this.authService.getUser();
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  async login() {
+    this.errorMessage = null;
+    try {
+      await this.authService.login();
+      this.isLoggedIn = await this.authService.isAuthenticated();
+      if (this.isLoggedIn) this.user = await this.authService.getUser();
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  async logout() {
+    this.errorMessage = null;
+    try {
+      await this.authService.logout();
+      this.user = null;
+      this.isLoggedIn = false;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  private handleError(error: unknown) {
+    if (error instanceof Error) {
+      this.errorMessage = error.message;
+    } else {
+      this.errorMessage = 'An unknown error occurred';
+    }
+    console.error('Authentication error:', error);
+  }
+}
