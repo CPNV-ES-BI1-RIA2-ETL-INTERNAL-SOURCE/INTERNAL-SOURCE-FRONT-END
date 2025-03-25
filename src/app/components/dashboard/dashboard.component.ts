11a49a3cf5841da7cddbc8d87aaa5d74b7886641
JuilletMikael <mikael.juillet@eduvaud.ch>
2025-03-25 18:14:47 +0100

    feat: update dashbord with login message, no chart and dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartComponent } from '../chart/chart.component';
+import {NgForOf, NgIf} from '@angular/common';
+import { ChartService } from '../../services/chart/chart.service';
+import {AuthService} from '../../services/authentication/auth.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    ChartComponent,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  urls: string[] = [];
+  isAuthenticated: boolean = false;
+  isLoggedIn = false;
 
+  constructor(
+    private dashboardService: ChartService,
+    private authService: AuthService
+  ) {}
+
+  async ngOnInit() {
+    this.isLoggedIn = await this.authService.isAuthenticated();
+    console.log(this.isLoggedIn)
+    if (this.isAuthenticated) {
+      this.dashboardService.getChartsUrls().subscribe(urls => {
+        this.urls = urls;
+      });
+    }
+  }
 }
